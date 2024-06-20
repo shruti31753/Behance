d3c3de592a2770d6bd54e55a61c407c6a0f323ae
Shruti Shivhare <sshruti7080@gmail.com>
2024-06-20 17:40:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import './index.css';
+import './App.css';
+import Header from './components/Header';
+import BehanceList from './components/BehanceList';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+    <div className="main relative overflow-hidden">
+
+    <Header/>
+    <BehanceList/>
+    <Footer/>
+
+    
+    </div>
+    </>
+  );
+}
+
+export default App;
